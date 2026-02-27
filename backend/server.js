a0bbcb888e29b4866ee3219cb2b07e09a6a38460
Adal Seju <shimahhshimuu@gmail.com>
2026-02-27 14:53:54 +0530

    changes ver02

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
@@ -21,3 +24,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
