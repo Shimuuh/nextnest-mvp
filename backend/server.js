a65f6956e2eecaf8fbfbba209903d20d1bed69f6
Adal Seju <shimahhshimuu@gmail.com>
2026-02-27 17:21:56 +0530

    changes ver07

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
+  console.log('Address:', server.address());
 });
 
